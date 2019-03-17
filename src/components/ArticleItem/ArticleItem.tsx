59348513078510645478e62ab328b403ad9706cb
Elie Steinbock <eliesteinbock@gmail.com>
2019-03-17 02:58:45 +0200

    working on article styling. desktop wip

--- src/components/ArticleItem/ArticleItem.tsx
+++ src/components/ArticleItem/ArticleItem.tsx
@@ -1,52 +1,70 @@
 import * as React from "react"
 import styled from "@emotion/styled"
 
+const MOBILE_BREAK_POINT = 500
+
 const Wrapper = styled.a`
   display: flex;
+  @media (max-width: ${MOBILE_BREAK_POINT}px) {
+    flex-direction: column;
+  }
   border-radius: 5px;
   border: solid 1px #ececec;
   background-color: #f4f7f6;
   color: #000000;
-  margin-bottom: 30px;
+  /* margin-bottom: 30px; */
   line-height: 1.42857em;
-  height: 200px;
+  /* height: 200px; */
   cursor: pointer;
   text-decoration: none;
+  overflow: hidden;
+  width: 100%;
 
   :hover {
     border: solid 1px #288ade;
   }
 `
 const Image = styled.img`
-  width: 364px;
+  width: 159px;
+  height: 124px;
   object-fit: cover;
-  @media (max-width: 1000px) {
+  @media (max-width: ${MOBILE_BREAK_POINT}px) {
+    width: 100%;
+    height: 100px;
+  }
+  /* @media (max-width: 1000px) {
     width: 250px;
   }
-  @media (max-width: 600px) {
+  @media (max-width: ${MOBILE_BREAK_POINT}px) {
     width: 150px;
-  }
+  } */
 `
 const Main = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  padding: 28px 43px 19px;
+  padding: 18px 22px;
+  justify-content: space-between;
+
+  @media (max-width: ${MOBILE_BREAK_POINT}px) {
+    padding: 16px 19px;
+  }
 `
 const Title = styled.h3`
-  font-size: 24px;
-  font-weight: 800;
-`
-const Description = styled.div`
   font-size: 18px;
-  flex: 1;
-  padding-top: 8px;
-  max-height: 100px;
-  overflow: hidden;
+  font-weight: 800;
+  margin-bottom: 12px;
 `
+// const Description = styled.div`
+//   font-size: 14px;
+//   flex: 1;
+//   padding-top: 8px;
+//   max-height: 100px;
+//   overflow: hidden;
+// `
 const Bottom = styled.div`
   display: flex;
-  font-size: 18px;
+  font-size: 16px;
 `
 const Likes = styled.div`
   margin-right: 43px;
@@ -59,11 +77,12 @@ const PostedAt = styled.div``
 export interface ArticleItemProps extends React.HTMLProps<HTMLDivElement> {
   title: string
   link: string
-  description: string
   image: string
   date: Date
   tags: string[]
   likes?: number
+  // removed
+  description?: string
 }
 
 export default (props: ArticleItemProps) => {
@@ -72,10 +91,10 @@ export default (props: ArticleItemProps) => {
       <Image src={props.image} />
       <Main>
         <Title>{props.title}</Title>
-        <Description>
+        {/* <Description>
           {props.description.substr(0, 140)}
           {props.description.length >= 140 && "..."}
-        </Description>
+        </Description> */}
         <Bottom>
           <Likes>{props.likes || 0} likes</Likes>
           <Tags>{props.tags.map((tag) => `#${tag.toLowerCase()}`).join(", ")}</Tags>
--- src/components/ArticleList/ArticleList.tsx
+++ src/components/ArticleList/ArticleList.tsx
@@ -4,6 +4,16 @@ import ArticleItem, { ArticleItemProps } from "../ArticleItem/ArticleItem"
 
 const Wrapper = styled.div`
   width: 100%;
+  /* display: flex;
+  flex-wrap: wrap; */
+  display: grid;
+  grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+  @media (max-width: 1100px) {
+    grid-template-columns: 1fr;
+  }
+  justify-items: center;
+  grid-row-gap: 25px;
+  grid-column-gap: 25px;
 `
 
 interface ArticleListProps {
