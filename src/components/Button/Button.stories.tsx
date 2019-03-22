dfd7b18dc5e5af33a5728b5438ab771bfd6d0015
Elie Steinbock <eliesteinbock@gmail.com>
2019-03-22 15:54:23 +0200

    added knobs example

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,12 +1,17 @@
 import * as React from "react"
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
+import { withKnobs, text } from "@storybook/addon-knobs"
 import Button from "./Button"
 
-storiesOf("Button", module)
-  .add("default", () => <Button onClick={action("clicked")}>CLICK ME</Button>)
+const stories = storiesOf("Button", module)
+
+stories.addDecorator(withKnobs)
+
+stories
+  .add("default", () => <Button onClick={action("clicked")}>{text("Text", "CLICK ME")}</Button>)
   .add("full", () => (
     <Button full onClick={action("clicked")}>
-      CLICK ME
+      {text("Text", "CLICK ME")}
     </Button>
   ))
