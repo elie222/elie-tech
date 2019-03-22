e3fca1b17b34b58d1b6f60511ed2e94a40a8aaf2
Elie Steinbock <eliesteinbock@gmail.com>
2019-03-22 16:13:05 +0200

    video sb knobs

--- src/components/VideoItem/VideoItem.stories.tsx
+++ src/components/VideoItem/VideoItem.stories.tsx
@@ -1,12 +1,15 @@
 import * as React from "react"
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
+import { withKnobs, text } from "@storybook/addon-knobs"
 import VideoItem from "./VideoItem"
 
-storiesOf("VideoItem", module).add("default", () => {
-  const title = "Great Video"
-  const image = "TODO"
-  const link = "#"
+storiesOf("VideoItem", module)
+  .addDecorator(withKnobs)
+  .add("default", () => {
+    const title = text("Title", "Great Video")
+    const link = text("Link", "#")
+    const image = "TODO"
 
-  return <VideoItem title={title} image={image} link={link} />
-})
+    return <VideoItem title={title} image={image} link={link} />
+  })
