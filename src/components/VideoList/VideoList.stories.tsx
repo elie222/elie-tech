9746d8d735f1f498eb7e8fde7f658a809787fe0d
Elie Steinbock <eliesteinbock@gmail.com>
2019-03-22 17:19:04 +0200

    fixed video list

--- src/components/VideoList/VideoList.stories.tsx
+++ src/components/VideoList/VideoList.stories.tsx
@@ -1,13 +1,23 @@
 import * as React from "react"
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
+import { withKnobs, text, number } from "@storybook/addon-knobs"
 import VideoList from "./VideoList"
 
 storiesOf("VideoList", module)
+  .addDecorator(withKnobs)
   .add("default", () => {
-    const videos: any[] = null // TODO assign real value
+    const videos = []
 
-    return (
-      <VideoList videos={videos} />
-    )
+    const numberOfVideos = number("Number of videos", 3)
+
+    for (let i = 0; i < numberOfVideos; i++) {
+      videos.push({
+        title: `Great Video #${i}`,
+        link: "#",
+        image: "TODO",
+      })
+    }
+
+    return <VideoList videos={videos} />
   })
