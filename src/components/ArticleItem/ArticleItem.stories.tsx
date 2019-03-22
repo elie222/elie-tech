6655d883216d728c3358b111a54f65ec86dbc05a
Elie Steinbock <eliesteinbock@gmail.com>
2019-03-22 16:04:25 +0200

    added knobs to sv

--- src/components/ArticleItem/ArticleItem.stories.tsx
+++ src/components/ArticleItem/ArticleItem.stories.tsx
@@ -1,18 +1,21 @@
 import * as React from "react"
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
+import { withKnobs, text, number } from "@storybook/addon-knobs"
 import ArticleItem from "./ArticleItem"
 import "../../css/reset.css"
 
-storiesOf("ArticleItem", module).add("default", () => {
-  const title: string = "Scaling Meteor"
-  const date: string = "Dec 19"
-  const tags: string[] = ["JavaScript", "Meteor"]
-  const image: string = "/images/TODO"
-  const likes = 186
-  const link = "#"
+storiesOf("ArticleItem", module)
+  .addDecorator(withKnobs)
+  .add("default", () => {
+    const title = text("Title", "Learning Storybook")
+    const date = text("Date", "Dec 19")
+    const tags = ["JavaScript", "Meteor"]
+    const image = text("Image", "/images/TODO")
+    const likes = number("Likes", 1012)
+    const link = text("Link", "#")
 
-  return (
-    <ArticleItem title={title} date={date} tags={tags} image={image} likes={likes} link={link} />
-  )
-})
+    return (
+      <ArticleItem title={title} date={date} tags={tags} image={image} likes={likes} link={link} />
+    )
+  })
--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -4,11 +4,8 @@ import { action } from "@storybook/addon-actions"
 import { withKnobs, text } from "@storybook/addon-knobs"
 import Button from "./Button"
 
-const stories = storiesOf("Button", module)
-
-stories.addDecorator(withKnobs)
-
-stories
+storiesOf("Button", module)
+  .addDecorator(withKnobs)
   .add("default", () => <Button onClick={action("clicked")}>{text("Text", "CLICK ME")}</Button>)
   .add("full", () => (
     <Button full onClick={action("clicked")}>
