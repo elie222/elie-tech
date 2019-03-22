e1dbf6a97a6c7c2936dbaf87027ee81438b62fa4
Elie Steinbock <eliesteinbock@gmail.com>
2019-03-22 17:26:56 +0200

    updated technology list sb

--- src/components/TechnologyItem/TechnologyItem.stories.tsx
+++ src/components/TechnologyItem/TechnologyItem.stories.tsx
@@ -2,10 +2,21 @@ import * as React from "react"
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
 import TechnologyItem from "./TechnologyItem"
+import { text, number, boolean } from "@storybook/addon-knobs"
 
 storiesOf("TechnologyItem", module).add("default", () => {
-  const title: string = "React"
-  const image: string = "/images/xxx" // TODO assign real value
+  const title = "React"
+  const image = "TODO"
+  const imageWidth = 120
+  const showLabel = true
 
-  return <TechnologyItem title={title} image={image} showLabel imageWidth={100} />
+  // not working
+  // const title = text('Title', 'React')
+  // const image = 'TODO'
+  // const imageWidth = number('Image width', 120)
+  // const showLabel = boolean('Show Label', true)
+
+  return (
+    <TechnologyItem title={title} image={image} showLabel={showLabel} imageWidth={imageWidth} />
+  )
 })
