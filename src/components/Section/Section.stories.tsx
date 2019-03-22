330d3becfede9b85f90b10a988fac24c7dedfee2
Elie Steinbock <eliesteinbock@gmail.com>
2019-03-22 15:32:42 +0200

    coloured background section sb

--- src/components/Section/Section.stories.tsx
+++ src/components/Section/Section.stories.tsx
@@ -3,4 +3,6 @@ import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
 import Section from "./Section"
 
-storiesOf("Section", module).add("default", () => <Section>Some content</Section>)
+storiesOf("Section", module)
+  .add("default", () => <Section>Some content</Section>)
+  .add("colored background", () => <Section coloredBackground>Some content</Section>)
