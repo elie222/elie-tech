bb7207ba72a199fe028db09165c9bbc7b60b66b9
Elie Steinbock <eliesteinbock@gmail.com>
2019-03-22 17:24:14 +0200

    added technology list

--- src/components/TechnologyList/TechnologyList.stories.tsx
+++ src/components/TechnologyList/TechnologyList.stories.tsx
@@ -1,10 +1,26 @@
 import * as React from "react"
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
+import { boolean, number } from "@storybook/addon-knobs"
 import TechnologyList from "./TechnologyList"
+import { TechnologyItemProps } from "../TechnologyItem/TechnologyItem"
 
 storiesOf("TechnologyList", module).add("default", () => {
-  const technologies: any[] = [] // TODO assign real value
+  const imageWidth = 120
+  const showLabel = true
 
-  return <TechnologyList technologies={technologies} showLabel imageWidth={100} />
+  // not working
+  // const imageWidth = number('Image width', 120)
+  // const showLabel = boolean('Show Label', true)
+
+  const technologies: TechnologyItemProps[] = [
+    { title: "Tech 1", image: "TODO", showLabel, imageWidth },
+    { title: "Tech 2", image: "TODO", showLabel, imageWidth },
+    { title: "Tech 3", image: "TODO", showLabel, imageWidth },
+    { title: "Tech 4", image: "TODO", showLabel, imageWidth },
+  ]
+
+  return (
+    <TechnologyList technologies={technologies} showLabel={showLabel} imageWidth={imageWidth} />
+  )
 })
